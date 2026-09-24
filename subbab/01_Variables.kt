eb56fd56548261d3a7e75322bda5b006f9ec1853
Ramiza Kamala Tatsuru <mizarakamala@gmail.com>
2026-09-24 18:12:04 +0700

    Variables

---
+++ subbab/01_Variables.kt
@@ -0,0 +1,15 @@
+fun main() {
+    // variabel read-only dengan val
+    val nama = "Kotlin"
+    val versi = "2.4.20"
+
+    // variabel yang dapat diubah dengan var
+    var jumlahBaris = 5
+
+    // untuk menetapkan nilai, gunakan operator penugasan =
+    jumlahBaris = jumlahBaris + 2
+
+    println(nama)
+    println(versi)
+    println(jumlahBaris)
+}
